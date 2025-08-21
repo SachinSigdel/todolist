a69f850e70b9b3852c360c982dd93f54111b788b
SachinSigdel <sigdelsachin08@gmail.com>
2025-08-21 19:43:12 +0545

    test commit

--- todo.py
+++ todo.py
@@ -2,7 +2,7 @@ from tabulate import tabulate
 
 class TodoList:
     # class to create object of every task and pass it to dictionary later
-    task_id = 1
+    task_id = 1 #id given to particular task
     def __init__(self,tasks,complete_status):
         self.task_id = TodoList.task_id
         self.tasks = tasks
